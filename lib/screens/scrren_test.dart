32b698c04af167e429202f9c0d27596df358b892
Kshitiz Dayal <kshitizdayal14@gmail.com>
2022-11-27 19:53:08 +0530

    testscreen

--- lib/screens/scrren_test.dart
+++ lib/screens/scrren_test.dart
@@ -1,11 +1,14 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class TestScreen extends StatelessWidget {
   const TestScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: CircularProgressIndicator(),
+      ),
+    );
   }
 }
